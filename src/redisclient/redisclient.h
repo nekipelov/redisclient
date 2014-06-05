69b4c7b60d3c72e7574ed08dffc2f0e76f0d8eca
Alex Nekipelov <alex@nekipelov.net>
2014-06-05 23:30:59 +0400

    Add comments

--- src/redisclient/redisclient.h
+++ src/redisclient/redisclient.h
@@ -21,7 +21,7 @@ class RedisClientImpl;
 
 class RedisClient : boost::noncopyable {
 public:
-    /// Subscribe handle.
+    // Subscribe handle.
     struct Handle {
         size_t id;
         std::string channel;
@@ -30,42 +30,53 @@ public:
     REDIS_CLIENT_DECL RedisClient(boost::asio::io_service &ioService);
     REDIS_CLIENT_DECL ~RedisClient();
 
+    // Connect to redis server, blocking call.
     REDIS_CLIENT_DECL bool connect(const char *address, int port);
 
+    // Connect to redis server, asynchronous call.
     REDIS_CLIENT_DECL void asyncConnect(
             const boost::asio::ip::tcp::endpoint &endpoint,
             const boost::function<void(const boost::system::error_code &)> &handler);
 
-    REDIS_CLIENT_DECL void installErrorHandler(const boost::function<void(const std::string &)> &handler);
+    // Set custom error handler. 
+    REDIS_CLIENT_DECL void installErrorHandler(
+        const boost::function<void(const std::string &)> &handler);
 
+    // Execute command on Redis server.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with one argument.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with two arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1, const std::string &arg2,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with three arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1,
             const std::string &arg2, const std::string &arg3,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with four arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1, const std::string &arg2,
             const std::string &arg3, const std::string &arg4,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with five arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1,
             const std::string &arg2, const std::string &arg3,
             const std::string &arg4, const std::string &arg5,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with six arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1,
             const std::string &arg2, const std::string &arg3,
@@ -74,6 +85,7 @@ public:
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
 
+    // Execute command on Redis server with seven arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::string &arg1,
             const std::string &arg2, const std::string &arg3,
@@ -81,23 +93,31 @@ public:
             const std::string &arg6, const std::string &arg7,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Execute command on Redis server with the list of arguments.
     REDIS_CLIENT_DECL void command(
             const std::string &cmd, const std::list<std::string> &args,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Subscribe to channel. Handler msgHandler will be called
+    // when someone publish message on channel. Call unsubscribe 
+    // to stop the subscription.
     REDIS_CLIENT_DECL Handle subscribe(
-            const std::string &channel,
+            const std::string &channelName,
             const boost::function<void(const std::string &msg)> &msgHandler,
-
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Unsubscribe
     REDIS_CLIENT_DECL void unsubscribe(const Handle &handle);
 
+    // Subscribe to channel. Handler msgHandler will be called
+    // when someone publish message on channel; it will be 
+    // unsubscribed after call.
     REDIS_CLIENT_DECL void singleShotSubscribe(
             const std::string &channel,
             const boost::function<void(const std::string &msg)> &msgHandler,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
 
+    // Publish message on channel.
     REDIS_CLIENT_DECL void publish(
             const std::string &channel, const std::string &msg,
             const boost::function<void(const RedisValue &)> &handler = &dummyHandler);
--- src/redisclient/redisvalue.h
+++ src/redisclient/redisvalue.h
@@ -20,15 +20,29 @@ public:
     REDIS_CLIENT_DECL RedisValue(const std::string &s);
     REDIS_CLIENT_DECL RedisValue(const std::vector<RedisValue> &array);
 
-    REDIS_CLIENT_DECL std::vector<RedisValue> toArray() const;
+    // Return the value as a std::string if 
+    // type is a std::string; otherwise returns an empty std::string.
     REDIS_CLIENT_DECL std::string toString() const;
+    
+    // Return the value as a std::vector<RedisValue> if 
+    // type is an int; otherwise returns 0.
     REDIS_CLIENT_DECL int toInt() const;
+    
+    // Return the value as an array if type is an array;
+    // otherwise returns an empty array.
+    REDIS_CLIENT_DECL std::vector<RedisValue> toArray() const;
 
+    // Return the string representation of the value. Use
+    // for dump content of the value.
     REDIS_CLIENT_DECL std::string inspect() const;
 
+    // Return true if this is a null.
     REDIS_CLIENT_DECL bool isNull() const;
+    // Return true if type is an int
     REDIS_CLIENT_DECL bool isInt() const;
+    // Return true if type is a string
     REDIS_CLIENT_DECL bool isString() const;
+    // Return true if type is an array
     REDIS_CLIENT_DECL bool isArray() const;
 
     REDIS_CLIENT_DECL bool operator == (const RedisValue &rhs) const;
